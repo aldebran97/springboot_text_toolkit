86e0ead755ac04ff62d9b0ccc3e8815fb1961296
Aldebran <525631870@qq.com>
2023-06-21 20:58:20 +0800

    update something

--- src/test/java/com/aldebran/text/ImportDataTest.java
+++ src/test/java/com/aldebran/text/ImportDataTest.java
@@ -39,7 +39,7 @@ public class ImportDataTest {
 
     public void importWikiJsons(File jsonsFolder, File libsFolder, String libName) throws Exception {
         TextSimilaritySearch lib = new TextSimilaritySearch(
-                libName, 1, 0.5, 2, 0.3, 10);
+                libName, 3, 0.5, 2, 0.01, 10);
         int c = 0;
         for (File file : jsonsFolder.listFiles()) {
             if (file.getName().endsWith(".json") && !file.getName().startsWith(".")) {
@@ -57,8 +57,8 @@ public class ImportDataTest {
     // -Xss
     @Test
     public void tryImport() throws Exception {
-        importWikiJsons(new File("/Users/aldebran/custom/data/wiki/wiki_jsons1_segment"),
-                new File("/Users/aldebran/custom/data/wiki/libs"), "wiki_jsons1_lib");
+        importWikiJsons(new File("/Users/aldebran/custom/data/wiki/wiki_interesting_segment"),
+                new File("/Users/aldebran/custom/data/wiki/libs"), "wiki_interesting_lib");
 
     }
 
@@ -66,21 +66,20 @@ public class ImportDataTest {
     public void tryLoadSearch() throws Exception {
         long loadSt = System.currentTimeMillis();
         TextSimilaritySearch lib = TextSimilaritySearch.load(
-                new File("/Users/aldebran/custom/data/wiki/libs", "wiki_jsons1_lib"));
+                new File("/Users/aldebran/custom/data/wiki/libs", "wiki_interesting_lib"));
         long loadEd = System.currentTimeMillis();
         System.out.println("load time: " + (loadEd - loadSt) / 1000.0 + "s");
 
-        lib.decayRate = 0.01;
-        lib.growthRate = 10;
-        lib.b = -1;
-        lib.a = (1.0 / (lib.criticalScore - 1) - lib.b) / (lib.avgIdf * lib.criticalHitCount +
-                lib.criticalHitCount * 0.7 * (lib.growthRate * lib.avgIdf + lib.avgIdf) / 2);
+        lib.regenerateArgs(3, 0.5, 0.01, 5);
+
         List<SimilaritySearchResult> resultList = null;
         int times = 1;
+
         long searchSt = System.currentTimeMillis();
         for (int i = 0; i < times; i++) {
-            resultList = lib.similaritySearch("木卫二(欧罗巴)是直径和质量第四大，公转轨道距离木星第六近的一颗。介绍木卫二", 30);
-//            resultList = lib.similaritySearch("介绍岳飞写的《满江红》", 10);
+//            resultList = lib.similaritySearch("木卫二(欧罗巴)是直径和质量第四大，公转轨道距离木星第六近的一颗。介绍木卫二", 10);
+//            resultList = lib.similaritySearch("介绍岳飞写的《满江红》(怒发冲冠凭栏处)", 10);
+            resultList = lib.similaritySearch("孟浩然的诗 春眠不觉晓", 10);
         }
         long searchEd = System.currentTimeMillis();
         for (SimilaritySearchResult similaritySearchResult : resultList) {
@@ -88,6 +87,7 @@ public class ImportDataTest {
         }
         System.out.println("search time: " + (searchEd - searchSt) / 1000.0 / times + "s");
         System.out.println("texts count: " + lib.textsCount());
+        System.out.println("grams count: " + lib.wordsCount());
 
     }
 
@@ -99,7 +99,7 @@ public class ImportDataTest {
         long loadEd = System.currentTimeMillis();
         System.out.println("load time: " + (loadEd - loadSt) / 1000.0 + "s");
         List<AC.MatchResult> mrs = null;
-        int times = 1;
+        int times = 20;
         long searchSt = System.currentTimeMillis();
         for (int i = 0; i < times; i++) {
             mrs = lib.ac.indexOf("木卫二欧罗巴直径和质量第四大公转轨道距离木星第六一颗介绍木卫二");

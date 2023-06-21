482da4e6e61dc68bc126b1234dbd63b984cea2f9
Aldebran <525631870@qq.com>
2023-06-21 18:33:20 +0800

    update title score

--- src/test/java/com/aldebran/text/ImportDataTest.java
+++ src/test/java/com/aldebran/text/ImportDataTest.java
@@ -1,6 +1,7 @@
 package com.aldebran.text;
 
 import cn.hutool.core.io.FileUtil;
+import com.aldebran.text.ac.AC;
 import com.aldebran.text.similarity.SimilaritySearchResult;
 import com.aldebran.text.similarity.TextSimilaritySearch;
 import com.alibaba.fastjson.JSON;
@@ -71,14 +72,40 @@ public class ImportDataTest {
 
         lib.decayRate = 0.01;
         lib.growthRate = 10;
+        lib.b = -1;
+        lib.a = (1.0 / (lib.criticalScore - 1) - lib.b) / (lib.avgIdf * lib.criticalHitCount +
+                lib.criticalHitCount * 0.7 * (lib.growthRate * lib.avgIdf + lib.avgIdf) / 2);
+        List<SimilaritySearchResult> resultList = null;
+        int times = 1;
         long searchSt = System.currentTimeMillis();
-        List<SimilaritySearchResult> resultList = lib.similaritySearch("木卫二(欧罗巴)是直径和质量第四大，公转轨道距离木星第六近的一颗。介绍木卫二", 10);
+        for (int i = 0; i < times; i++) {
+            resultList = lib.similaritySearch("木卫二(欧罗巴)是直径和质量第四大，公转轨道距离木星第六近的一颗。介绍木卫二", 30);
+//            resultList = lib.similaritySearch("介绍岳飞写的《满江红》", 10);
+        }
         long searchEd = System.currentTimeMillis();
         for (SimilaritySearchResult similaritySearchResult : resultList) {
             System.out.println(similaritySearchResult);
         }
-        System.out.println("search time: " + (searchEd - searchSt) / 1000.0 + "s");
+        System.out.println("search time: " + (searchEd - searchSt) / 1000.0 / times + "s");
         System.out.println("texts count: " + lib.textsCount());
 
+    }
+
+    @Test
+    public void acTest() throws Exception {
+        long loadSt = System.currentTimeMillis();
+        TextSimilaritySearch lib = TextSimilaritySearch.load(
+                new File("/Users/aldebran/custom/data/wiki/libs", "wiki_jsons1_lib"));
+        long loadEd = System.currentTimeMillis();
+        System.out.println("load time: " + (loadEd - loadSt) / 1000.0 + "s");
+        List<AC.MatchResult> mrs = null;
+        int times = 1;
+        long searchSt = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            mrs = lib.ac.indexOf("木卫二欧罗巴直径和质量第四大公转轨道距离木星第六一颗介绍木卫二");
+        }
+        long searchEd = System.currentTimeMillis();
+        System.out.println("search time: " + (searchEd - searchSt) / 1000.0 / times + "s");
+        System.out.println(mrs);
     }
 }
